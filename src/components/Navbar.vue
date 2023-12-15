dad62bc9fc86ba4bc1d8ed8dea5b85a2e78352d7
Ali Babaei <alibabaeian670@gmail.com>
2023-12-15 14:36:15 +0330

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <span>ToeDoe</span>
+                <strong>List</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="my-tasks.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Summary</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-outline-secondary ms-2">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-danger ms-2">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
