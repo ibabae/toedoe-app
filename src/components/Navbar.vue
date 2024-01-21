8d2cfc56c254aa2d76baf43f7a817b4e0008d2ab
Ali Babaei <alibabaeian670@gmail.com>
2024-01-21 14:24:56 +0330

    003 Programmatic navigations

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,8 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <router-link :to="{name: 'tasks'}" class="nav-link">Tasks</router-link>
+                        <!-- <router-link :to="{name: 'tasks'}" class="nav-link">Tasks</router-link> -->
+                        <a href="#" @click.prevent="$event => $router.push({name: 'tasks'})" class="nav-link">Tasks</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Summary</a>
