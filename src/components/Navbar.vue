8e9ecd0c577da5a8c78fb68bc721abef073dd14d
Ali Babaei <alibabaeian670@gmail.com>
2024-01-21 14:16:45 +0330

    route navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="my-tasks.html">Home</a>
+                        <router-link :to="{name: 'tasks'}" class="nav-link">Tasks</router-link>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Summary</a>
@@ -19,10 +19,10 @@
                 </ul>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a href="#" class="btn btn-outline-secondary ms-2">Login</a>
+                        <router-link :to="{name: 'login'}" class="btn btn-outline-secondary ms-2">Login</router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="btn btn-danger ms-2">Register</a>
+                        <router-link :to="{name: 'register'}" class="btn btn-danger ms-2">Register</router-link>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
